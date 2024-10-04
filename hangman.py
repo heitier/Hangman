3f094858626abf980b54800f56b5a228572c959e
Heritier <matetah@gmail.com>
2024-10-03 20:46:07 -0400

    second commit

--- hangman.py
+++ hangman.py
@@ -1,6 +1,8 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel", "cattle", "dog", "elephant", "fish", "goat", "kangaroo", "hamster"]
+lives = 6
+tracked_lives = 0
 
 random_word = random.choice(word_list)
 # print (random_word)
@@ -16,22 +18,37 @@ game_over = False
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
-    prev_g.append(guess)
-    display = []
-    for letter in random_word:
-        if letter in prev_g:
-            display.append(letter)
-        else:
-            display.append("_")
+
+    if guess in prev_g:
+        print("You already guessed that letter!")
+        tracked_lives += 1
+
+    else:
+        prev_g.append(guess)
+        display = []
+
+        for letter in random_word:
+            if letter in prev_g:
+                display.append(letter)
+            else:
+                display.append("_")
+
+        if guess not in random_word :
+            tracked_lives += 1
+
+
     display_str = "".join(display)
     print(display_str)
+    print(f"Lives remaining: {lives - tracked_lives}")
+
 
     #print(display)
     if "_" not in display:
         game_over = True
         print("You win!")
-
-
+    elif lives - tracked_lives == 0:
+        game_over = True
+        print("You lose!")
 
 
 
